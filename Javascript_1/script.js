09463ee47d375f70209557c00f8473b1609d8727
DaiHoang123 <daihoangktv@gmail.com>
2024-08-16 19:05:19 +0700

    Javascript Bai 1

---
+++ Javascript_1/script.js
@@ -0,0 +1,11 @@
+const message = 'Hello world'
+
+const changeContent = () => {
+  document.querySelector('#header').innerHTML = message
+}
+
+console.log(message)
+
+const showAlert = () => {
+  alert("Thong bao ABC!");
+};
